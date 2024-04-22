b08fca7c1568028c479b57fd43a7224d63f7c5f8
Ricardo Meireles <cadomeireles@gmail.com>
2024-04-22 20:38:28 -0300

    Create a healthcheck endpoint

---
+++ cmd/api/main.go
@@ -0,0 +1,60 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"log/slog"
+	"net/http"
+	"os"
+	"time"
+)
+
+const version = "1.0.0"
+
+type config struct {
+	port int
+	env  string
+}
+
+type application struct {
+	config config
+	logger *slog.Logger
+}
+
+func main() {
+	var conf config
+
+	// cmd-line options
+	flag.IntVar(&conf.port, "port", 8000, "Application port.")
+	flag.StringVar(&conf.env, "env", "dev", "Application environment.")
+	flag.Parse()
+
+	// set the logger
+	logger := slog.New(slog.NewTextHandler(os.Stdout, nil))
+
+	app := &application{
+		config: conf,
+		logger: logger,
+	}
+
+	// initiate requests multiplexer
+	mux := http.NewServeMux()
+	mux.HandleFunc("/v1/healthcheck", app.healthcheckHandler)
+
+	// define server settings
+	server := &http.Server{
+		Addr:         fmt.Sprintf(":%d", conf.port),
+		Handler:      mux,
+		IdleTimeout:  time.Minute,
+		ReadTimeout:  5 * time.Second,
+		WriteTimeout: 10 * time.Second,
+		ErrorLog:     slog.NewLogLogger(logger.Handler(), slog.LevelError),
+	}
+
+	logger.Info("Starting server...", "addr", server.Addr, "env", conf.env)
+
+	// start server
+	err := server.ListenAndServe()
+	logger.Error(err.Error())
+	os.Exit(1)
+}
